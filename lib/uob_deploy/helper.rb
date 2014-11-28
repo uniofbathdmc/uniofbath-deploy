0bdf228e635a904b3898af1c9f63b18854b09ac6
ma1twn <T.W.Natt@bath.ac.uk>
2014-11-28 15:07:40 +0000

    added some directory creation code

--- lib/uob_deploy/helper.rb
+++ lib/uob_deploy/helper.rb
@@ -1,11 +1,30 @@
 class UobDeploy
+
   # create the deploy.rb file and deployments directory
   def self.init
-    puts "init"
+    puts "Checking config directory"
+    unless Dir.exists?('config')
+      puts "Creating directory"
+      Dir.mkdir('config')
+    end
+
+    puts "Creating config/deploy.rb"
+
+    puts "Checking config/deployments directory"
+    unless Dir.exists?('config/deployments')
+      puts "Creating directory"
+      Dir.mkdir('config/deployments')
+    end
+
+    puts "Creating basic deployment file"
+
+    puts "Done"
   end
 
   # create a new environment file
   def self.new
-    puts "new"
+    puts "Checking config/deployments"
+
+    puts "Creating basic deployment file"
   end
 end
