23db8c2c52c3c415d97b1f47ecf12abb05dff08f
ma1twn <T.W.Natt@bath.ac.uk>
2014-11-28 15:38:20 +0000

    fixed a path

--- lib/templates/deploy.rb
+++ lib/templates/deploy.rb
@@ -6,7 +6,7 @@ require 'mina/unicorn'
 require 'uob_deploy'
 
 # load the environment config files
-Dir['./config/deployment/*.rb'].each { |f| load f }
+Dir['./config/deployments/*.rb'].each { |f| load f }
 
 set :repository,   'git@github.bath.ac.uk:digital/cms-alpha-publisher.git'
 set :default_server, :digilin01
