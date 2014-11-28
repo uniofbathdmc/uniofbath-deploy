ba0312d01a3b29c43d1f1eb2d926a4cf8bc70d98
ma1twn <T.W.Natt@bath.ac.uk>
2014-11-28 15:30:00 +0000

    refactored template creation code

--- lib/uob_deploy/helper.rb
+++ lib/uob_deploy/helper.rb
@@ -2,24 +2,36 @@ require 'fileutils'
 
 class UobDeploy
 
+  # application locations
   @config_dir = 'config'
-  @deployments_dir = File.join(@config_dir, 'deployments')
+  @deploy_dir = File.join(@config_dir, 'deployments')
+
+  # templates
   @templates_dir = File.join(File.dirname(__FILE__), '../templates/')
-  @deployments_template = File.join(@templates_dir, 'digilin01.rb')
+  @unicorn_deploy_template_name = 'deploy.rb'
+  @environment_deploy_template_name = 'digilin01.rb'
+
+  # to copy
+  @unicorn_template = File.join(@templates_dir, @unicorn_deploy_template_name)
+  @environment_template = File.join(@templates_dir, @environment_deploy_template_name)
+
+  # output
+  @unicorn_deploy = File.join(@config_dir, @unicorn_deploy_template_name)
+  @environment_deploy = File.join(@config_dir, @environment_deploy_template_name)
 
   # create the deploy.rb file and deployments directory
   def self.init
     # create a config directory if there isn't one already
-    FileUtils.mkdir_p(@deployments_dir)
+    FileUtils.mkdir_p(@deploy_dir)
 
-    puts "Creating config/deploy.rb"
-    unless File.exists?(@deployments_template)
-      # FileUtils.cp(@deployments_template, @deployments_dir)
+    puts "Creating #{unicorn_deploy}"
+    unless File.exists?(@unicorn_deploy)
+      FileUtils.cp(@unicorn_template, @unicorn_deploy)
     end
 
-    puts "Creating basic deployment file"
-    unless File.exists?(@deployments_template)
-      FileUtils.cp(@deployments_template, @deployments_dir)
+    puts "Creating #{environment_deploy}"
+    unless File.exists?(@environment_deploy)
+      FileUtils.cp(@environment_template, @environment_deploy)
     end
 
     puts "Done"
