d514dba453b094b2daf818e1c6794ce90b035246
ma1twn <T.W.Natt@bath.ac.uk>
2014-11-28 15:50:45 +0000

    updated default environment to be more helpful

--- lib/templates/digilin01.rb
+++ lib/templates/digilin01.rb
@@ -1,13 +1,14 @@
 namespace :env do
   task :digilin01 do
     set :branch,       'mina-rewrite'          # branch to deploy
-    set :rails_env,    'staging'               # rails environment to use
+    set :rails_env,    'development'           # rails environment to use
     set :domain,       'digilin-01.bath.ac.uk' # machine
     set :user,         'ma1twn-test'           # user to deploy as
     set :deploy_to,    '/home/ma1twn-test'     # home directory of user
     set :app_port,     3055                    # port to run on
 
-    set :shared_paths, ['tmp', 'log', 'config/unicorn.rb', 'output']
+    # uncomment and add to this if you wish to change defaults
+    # set :shared_paths, ['tmp', 'log', 'config/unicorn.rb']
 
     # this needs to be here in every environment
     invoke :defaults
