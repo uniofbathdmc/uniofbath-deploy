988792329b893b49576b3b27dc7368a73075b1d4
ma1twn <T.W.Natt@bath.ac.uk>
2014-11-28 15:00:20 +0000

    rearrange

--- lib/uob_deploy.rb
+++ lib/uob_deploy.rb
@@ -1,17 +1,2 @@
 require 'uob_deploy/defaults'
 require 'uob_deploy/tasks'
-
-# load the templates
-# require File.join(File.dirname(__FILE__), '/templates/unicorn.rb.erb')
-
-class UobDeploy
-  # create the deploy.rb file and deployments directory
-  def self.init
-    puts "init"
-  end
-
-  # create a new environment file
-  def self.new
-    puts "new"
-  end
-end
---
+++ lib/uob_deploy/helper.rb
@@ -0,0 +1,11 @@
+class UobDeploy
+  # create the deploy.rb file and deployments directory
+  def self.init
+    puts "init"
+  end
+
+  # create a new environment file
+  def self.new
+    puts "new"
+  end
+end
